7a5d0e984bacb01369ea827b673ffc4b9ec00d42
MikhailVinogradov224 <m89119750476@gmail.com>
2022-04-24 02:11:43 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите число");
+int n = Console.Read();
+int i = 0;
+while (i <= n)
+{
+    if(i == n)
+        Console.Write($"{i} ");
+    else
+        Console.Write($"{i} ");
+    i+=2;
+}
